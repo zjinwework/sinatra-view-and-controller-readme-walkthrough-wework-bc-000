056f710954c79d846c62f46a0ee992d84689b25f
Zhen Jin <zhen.jin@wework.com>
2018-02-27 03:57:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,9 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
-    puts params
+    original_string = params["string"]
+    reversed_string = original_string.reverse
+    
     erb :reversed
   end
 
